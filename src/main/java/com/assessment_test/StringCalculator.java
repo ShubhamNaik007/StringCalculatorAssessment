a6f5f358ee45979849c041c54bc41b792ac090f6
ShubhamNaikVSH <shubham.naik@vshsolutions.com>
2024-10-26 12:10:09 +0530

    Write just enough code to make the single number string test pass

--- src/main/java/com/assessment_test/StringCalculator.java
+++ src/main/java/com/assessment_test/StringCalculator.java
@@ -3,12 +3,20 @@ package com.assessment_test;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-
     public int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
         }
+
+        String delimiter = ",|\n";
         int sum = 0;
+        String[] numArray = numbers.split(delimiter);
+        for (String num : numArray) {
+            if (!num.isEmpty()) {
+                int number = Integer.parseInt(num);
+                sum += number;
+            }
+        }
 
         return  sum;
     }
