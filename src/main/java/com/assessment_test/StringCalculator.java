f551221452dec68cf1582f66a8e3d9fc905f729a
ShubhamNaikVSH <shubham.naik@vshsolutions.com>
2024-10-26 11:51:12 +0530

    Write a failing test for a add functionality first

---
+++ src/main/java/com/assessment_test/StringCalculator.java
@@ -0,0 +1,10 @@
+package com.assessment_test;
+
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    public int add(String numbers){
+        return -1;
+    }
+}
---
+++ src/test/java/com/assessment_test/StringCalculatorTest.java
@@ -0,0 +1,16 @@
+package com.assessment_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class StringCalculatorTest {
+
+    private final StringCalculator calculator = new StringCalculator();
+
+    @Test
+    void add_emptyString(){
+        assertEquals(0,"");
+    }
+}
