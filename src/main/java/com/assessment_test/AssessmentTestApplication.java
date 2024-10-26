6a33867db51f146283b18bd2cfeedda93330c37c
ShubhamNaikVSH <shubham.naik@vshsolutions.com>
2024-10-26 11:31:21 +0530

    string calculator assessment template

---
+++ src/main/java/com/assessment_test/AssessmentTestApplication.java
@@ -0,0 +1,13 @@
+package com.assessment_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment_test/AssessmentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
